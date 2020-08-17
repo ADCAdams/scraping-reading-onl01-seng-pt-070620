55fa6f4f20464def8d8b9334a796bb0d09380d24
Andy Adams <andrewdcadams@gmail.com>
2020-08-17 12:28:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,18 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+html2= open("https://newyork.craigslist.org/search/sss?search_distance=1&postal=07302&max_price=1000")
+
+
+doc = Nokogiri::HTML(html)
+doc2 = Nokogiri::HTML(html2)
+puts doc2.css(".rows.result-row.result-title hdrlnk")
+
+
+
+doc.css(".headline-26OIBN").text
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+courses.each do |course|
+  puts course.text.strip
+end
