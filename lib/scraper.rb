a8f577a861eb8ae2fd583f173d44926aee924b1c
Andy Adams <andrewdcadams@gmail.com>
2020-08-17 12:48:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,7 +16,6 @@ end
 
 puts doc.css(".headline-26OIBN").text
 courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
-puts courses 
 
 courses.each do |course|
   puts course.text.strip
